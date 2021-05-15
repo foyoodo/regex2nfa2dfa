28342585cdfa2f1e84d5733543bee8e571234f38
foyoodo <foyoodo@gmail.com>
2021-05-16 00:22:17 +0800

    update

--- regex2nfa.cpp
+++ regex2nfa.cpp
@@ -23,6 +23,12 @@ typedef vector<vector<ArcNode *>> ALGraph;
 int cur = 0;
 stack<char> ops, vals;
 
+inline void increase(ALGraph &graph, int k) {
+    for (int i = graph.size(); i <= k; ++i) {
+        graph.push_back(vector<ArcNode *>());
+    }
+}
+
 void dealor(ALGraph &graph, int l, int r, char v) {
     int a = cur++;
     int b = cur++;
@@ -48,8 +54,17 @@ int dealand(ALGraph &graph, int l, char v) {
     return a;
 }
 
-void dealstar(ALGraph &graph, int l, int r) {
+void dealstar(ALGraph &graph, int &l, int &r) {
+    if (l == r) {
+        r = cur++;
+        increase(graph, r);
+    }
+    graph[l].push_back(new ArcNode('a', r));
     graph[r].push_back(new ArcNode('$', l));
+    l = cur++;
+    r = cur++;
+    increase(graph, r);
+    graph[l].push_back(new ArcNode('$', r));
 }
 
 void dealbracket(ALGraph &graph, int l, int r) {
@@ -73,15 +88,30 @@ void deal(ALGraph &graph, int l, int r) {
     }
 }
 
+bool isOperator(const char ch) {
+    if (ch == '(' || ch == ')' || ch == '|' || ch == '.' || ch == '*') {
+        return true;
+    }
+    return false;
+}
+
 bool build(ALGraph &graph, const string &s) {
     int left = cur++;
-    int right = cur++;
+    int right = left;
 
-    for (int i = graph.size(); i <= right; ++i) {
-        graph.push_back(vector<ArcNode *>());
-    }
+    increase(graph, left);
 
     for (int i = 0; i < s.size(); ++i) {
+        char c = s[i];
+        if (c == '(');
+        else if (c == '|') ops.push(c);
+        else if (c == '_') ops.push(c);
+        else if (c == '*') {
+
+        } else if (c == ')') {
+
+        } else vals.push(c);
+
         if (s[i] == '(' || s[i] == '|') {
             ops.push(s[i]);
         } else {
@@ -122,7 +152,7 @@ void order(ALGraph &graph) {
 
 int main(int argc, char const *argv[]) {
     ALGraph graph;
-    bool ret = build(graph, "(a|b)*abb");
+    bool ret = build(graph, "(a|b)*_a_b_b");
     order(graph);
     return 0;
 }
