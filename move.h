010ece4507cddf496039c811262e51632fedac8a
foyoodo <foyoodo@gmail.com>
2021-05-23 09:41:49 +0800

    move*

--- move.h
+++ move.h
@@ -13,6 +13,9 @@ struct Move {
     };
 
     Move(int _si, int _sj, char _ch) : si(_si), sj(_sj), ch(_ch) {}
+    Move(int _si, int _sj, vector<char> *_chs) : si(_si), sj(_sj) {
+        chs = new vector<char>(*_chs);
+    }
 };
 
 #endif
